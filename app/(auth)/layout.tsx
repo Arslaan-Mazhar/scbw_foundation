ca2d0499692627293d775a9b977fb29cee276db1
Arslaan-Mazhar <arslaanmazhar50@gmail.com>
2024-05-21 00:47:22 +0500

    clerk form fixed

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
-      <div className="flex-center items-center mt-24 ml-96 min-h-screen w-full bg-primary-50 bg-dotted-pattern bg-cover bg-fixed bg-center">
+      <div className="flex justify-center items-center h-screen w-full bg-primary-50 bg-dotted-pattern bg-cover bg-fixed bg-center">
         {children}
       </div>
     )
