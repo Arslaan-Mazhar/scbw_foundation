1194ed005ffc5e1360bb353e6e36bba5ae881319
Arslaan-Mazhar <arslaanmazhar50@gmail.com>
2024-06-05 23:39:15 +0500

    slider added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-
+import ImageSlider from "../../components/ImageSlider";
 export default function Home() {
   return (
     <main>
+      <div className="flex justify-center items-center h-screen">
+      <ImageSlider />
+      </div>
         <div className="flex justify-center mt-24 items-center h-screen">
         <Image 
             src="/img3.jpg" width={1000} height={800}
---
+++ components/ImageSlider.js
@@ -0,0 +1,44 @@
+"use client"
+// components/ImageSlider.js
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+// import SwiperCore, { Navigation, Pagination, Autoplay } from 'swiper/core';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+
+// SwiperCore.use([Navigation, Pagination, Autoplay]);
+
+const ImageSlider = () => {
+  return (
+    <Swiper
+    modules={[Navigation, Pagination, Autoplay]}
+      navigation
+      pagination={{ clickable: true }}
+      autoplay={{ delay: 3000, disableOnInteraction: false }}
+      loop={true}
+      spaceBetween={50}
+      slidesPerView={1}
+      style={{ width: '100%', height: '100%'  }}
+    >
+      <SwiperSlide>
+        <img src="/images/image1.jpg" alt="Image 1" style={{ width: '100%', height: '100%' }} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="/images/image2.jpg" alt="Image 2" style={{ width: '100%', height: '100%' }} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="/images/image3.jpg" alt="Image 3" style={{ width: '100%', height: '100%' }} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="/images/image4.jpg" alt="Image 4" style={{ width: '100%', height: '100%' }} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="/images/image5.jpg" alt="Image 5" style={{ width: '100%', height: '100%' }} />
+      </SwiperSlide>
+      {/* Add more slides as needed */}
+    </Swiper>
+  );
+};
+
+export default ImageSlider;
