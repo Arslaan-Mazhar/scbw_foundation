25dce3b77bdc4321391f24d1d96fad2521ec10bd
Arslaan-Mazhar <arslaanmazhar50@gmail.com>
2024-07-07 18:03:06 +0500

    middleware fixed

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,20 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware({
-    publicRoutes: [
-      '/',
-      '/aboutUs',
-      '/objectives',
-    //   '/events/:id',
+    // publicRoutes: [
+    //   '/',
+    //   '/aboutUs',
+    //   '/objectives',
+    // //   '/events/:id',
+    // //   '/api/webhook/clerk',
+    // //   '/api/webhook/stripe',
+    // //   '/api/uploadthing'
+    // ],
+    // ignoredRoutes: [
     //   '/api/webhook/clerk',
     //   '/api/webhook/stripe',
     //   '/api/uploadthing'
-    ],
-    ignoredRoutes: [
-      '/api/webhook/clerk',
-      '/api/webhook/stripe',
-      '/api/uploadthing'
-    ],
+    // ],
     
   });
 
