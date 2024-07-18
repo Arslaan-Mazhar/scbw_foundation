a312e0c071172e31a3484560ab9b6b6689521d78
Arslaan-Mazhar <arslaanmazhar50@gmail.com>
2024-07-18 17:34:40 +0500

    gui changes

--- app/(root)/aboutUs/page.tsx
+++ app/(root)/aboutUs/page.tsx
@@ -3,22 +3,22 @@ import Link from "next/link";
 
 const About = () => {
   return (
-    <section className="bg-gray-100 h-1/2 p-10 rounded-xl" >
-      <div className="flex flex-col  my-auto h-1/2 md:flex-row gap-2 items-stretch justify-center md: h-[29rem]">
-        <div className="flex-none w-full md:w-1/2  " >
+    <section className="bg-gray-100 p-10 rounded-xl " >
+      <div className="flex flex-col xl:flex-row gap-4 xl:mx-24 min-h-80 items-center justify-center ">
+        <div className="w-full xl:w-1/2  xl:  h-full" >
           <Link href="/"  >
             <Image
               src="/images/about-img.jpg"
-              width={800}
-              height={100}
+              width={900}
+              height={600}
               alt="About Us"
-              className="rounded-xl md: h-full"
+              className="rounded-xl h-[41.5rem] hidden xl:block"
             />
           </Link>
         </div>
-        <div className="flex-auto w-full md:w-1/2 md:h-full p-5 items-center justify-center text-center bg-slate-900 rounded-xl">
-           <h1 className="text-5xl text-white m-5">About Us</h1>
-           <p className="text-gray-50  p-5 text-justify">Special Campaign for Blind Welfere (SCBW) foundation is charity based organization which is launched to serve the blind and under privileged people of our society who, by any reason, have been left behind in life. The aim of this organization is to equip the blinds with modern knowledge and to pave the way of education for them.Moreover, it's a committed movement for the welfare of the blind and introducing them to advanced technology and a bright future.</p>
+        <div className="w-full xl:w-3/4 xl:h-[41.5rem] bg-slate-900 rounded-xl">
+           <h1 className="text-5xl text-white m-5 text-center lg:m-1">About Us</h1>
+           <p className="text-gray-50   text-justify text-3xl xl: p-10">Special Campaign for Blind Welfere (SCBW) foundation is charity based organization which is launched to serve the blind and under privileged people of our society who, by any reason, have been left behind in life. The aim of this organization is to equip the blinds with modern knowledge and to pave the way of education for them.Moreover, it's a committed movement for the welfare of the blind and introducing them to advanced technology and a bright future.</p>
         </div>
       </div>
     </section>
--- components/VideoSlider.tsx
+++ components/VideoSlider.tsx
@@ -2,15 +2,15 @@
 const VideoSlider = () => {
   return (
     <section className="bg-gray-100  p-10">
-      <div className="flex flex-col md:flex-row items-center justify-center ">
-         <div className="flex-none  w-full md:w-1/2 md:h-full">
-            <video width="800px" className="rounded-3xl p-2"  controls>
+      <div className="flex flex-col w-full  lg:flex-row gap-2 mx-24 items-center justify-center ">
+         <div className="w-full lg:mr-0 mr-48 md:h-full">
+            <video width="900px" className="rounded-3xl p-2"  controls>
               <source src="/videos/VID1.mp4" type="video/mp4" />
               Your browser does not support the video tag.
             </video>
           </div>
-          <div className="flex w-full md:w-1/2  md:h-full"> 
-            <video width="800px" className="rounded-xl"  controls>
+          <div className="w-full mr-48 lg:w-full  md:h-full"> 
+            <video width="900px" className="rounded-xl"  controls>
               <source src="/videos/VID2.mp4" type="video/mp4" />
               Your browser does not support the video tag.
             </video>
