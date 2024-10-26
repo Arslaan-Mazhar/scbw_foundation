f8ada1f3aa82dcc3b0a3d8fbb3862a78b9428a48
Arslaan-Mazhar <arslaanmazhar50@gmail.com>
2024-10-26 23:27:09 +0500

    sign-up page to home link

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -79,6 +79,14 @@ module.exports = withUt({
         sm: 'calc(var(--radius) - 4px)',
       },
       keyframes: {
+        'fade-in': {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        floating: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-20px)' },
+        },
         'accordion-down': {
           from: { height: '0' },
           to: { height: 'var(--radix-accordion-content-height)' },
@@ -91,6 +99,8 @@ module.exports = withUt({
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'fade-in': 'fade-in 1s ease-out',
+        floating: 'floating 6s ease-in-out infinite',
       },
     },
   },
