aea85fedea7eb5e8e3634eb4702908604580f738
Arslaan-Mazhar <arslaanmazhar50@gmail.com>
2024-12-09 01:08:03 +0500

    middleware added

--- _middleware.ts
+++ _middleware.ts
@@ -1,14 +1,14 @@
-// import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-// export function middleware(req: any) {
-//   const url = req.nextUrl.clone();
-//   const host = req.headers.get("host"); // Get the host (e.g., "admin.scbwfoundation.com")
+export function middleware(req: any) {
+  const url = req.nextUrl.clone();
+  const host = req.headers.get("host"); // Get the host (e.g., "admin.scbwfoundation.com")
 
-//   if (host?.startsWith("admin")) {
-//     // Redirect to admin page if "admin" subdomain
-//     url.pathname = "/login";
-//     return NextResponse.rewrite(url);
-//   }
+  if (host?.startsWith("admin")) {
+    // Redirect to admin page if "admin" subdomain
+    url.pathname = "www.scbwfoundation.org/login";
+    return NextResponse.rewrite(url);
+  }
 
-//   return NextResponse.next(); // Continue with the default behavior
-// }
+  return NextResponse.next(); // Continue with the default behavior
+}
